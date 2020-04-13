e2de8067d42eecb3f1e77efac1a3f1f1fffb3c1d
Antonio Alvarado <ant.36alv@gmail.com>
2020-04-13 11:18:07 -0400

    Initial commit to test https login on heroku.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,19 @@
 import React from "react";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+// import HelloWorld from "./components/HelloWorld"
+import Facebook from "./components/Facebook"//@HACER MOVE TO COMPONENETS
+// import { BrowserRouter, Route, Switch } from "react-router-dom";
 
 // The app will not render correctly until you setup a Route component.
 // Refer to the Basic Example documentation if you need to.
 // (https://reacttraining.com/react-router/web/example/basic)
 function App() {
   return (
-    <BrowserRouter>
-    <div>
-      <Nav />
-      <Switch>
-        <Route exact path="/" component = {Books} />
-        <Route exact path="/books" component = {Books} />
-        <Route exact path="/books/:id" component = {Detail} />
-        <Route component = {NoMatch}/>
-      </Switch>
-    </div>
-    </BrowserRouter>
+    <>
+    <hr />
+    <h1>FB Login</h1>
+    <Facebook />
+    <hr />
+    </>
   );
 }
 
