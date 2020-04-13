16ec4151c3abc8e02693b6e972dc2085df406da8
Chris Lee <cil5345@gmail.com>
2020-04-13 12:48:02 -0400

    added chat app

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,15 @@
 import React from "react";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
-import Nav from "./components/Nav";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Store from "./components/Store";
+import "./App.css";
 
-// The app will not render correctly until you setup a Route component.
-// Refer to the Basic Example documentation if you need to.
-// (https://reacttraining.com/react-router/web/example/basic)
 function App() {
   return (
-    <BrowserRouter>
-    <div>
-      <Nav />
-      <Switch>
-        <Route exact path="/" component = {Books} />
-        <Route exact path="/books" component = {Books} />
-        <Route exact path="/books/:id" component = {Detail} />
-        <Route component = {NoMatch}/>
-      </Switch>
+    <div className="App">
+      <Store>
+        <Dashboard />
+      </Store>
     </div>
-    </BrowserRouter>
   );
 }
 
