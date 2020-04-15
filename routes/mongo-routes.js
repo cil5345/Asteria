6321b034d56be83335d364d280517196ad41df73
Antonio Alvarado <ant.36alv@gmail.com>
2020-04-15 12:09:03 -0400

    now required comp.json

--- routes/mongo-routes.js
+++ routes/mongo-routes.js
@@ -11,6 +11,7 @@ module.exports = function(app) {
 
     //     bookController.findAll
     // })
+    
 
     app.post("/api/users", (req, res) => {
         
--- routes/test-routes.js
+++ routes/test-routes.js
@@ -1,8 +1,14 @@
 const userController = require("../controllers/userController")
-import comp from "../matches/comp.json"
+const comp = require("../matches/comp.json")
 
 
 module.exports = function(app)  {
+
+
+    app.get("/api/users/:id", (req, res) => {
+        
+        console.log("hit test routes get by id")
+    })
 
 
     app.get("/api/users", (req, res) => {
