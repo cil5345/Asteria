b360369a3f182c838335f72eb6e03a8c2c81dd89
Chris Lee <cil5345@gmail.com>
2020-04-15 08:09:25 -0400

    heroku push

--- socket.js
+++ socket.js
@@ -2,9 +2,9 @@ var app = require('express')();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
-app.get('/', function(req, res) {
-    res.send('<h1>Hello!!!!</h1>')
-});
+// app.get('/', function(req, res) {
+//     res.send('<h1>Hello!!!!</h1>')
+// });
 
 
 io.on('connection', function(socket){
