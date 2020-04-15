4e0ceab496063c5f161051807517771c6470c63b
Antonio Alvarado <ant.36alv@gmail.com>
2020-04-15 13:23:18 -0400

    testing to see if it creates user at first login, and doesnt on second login

--- client/src/components/Facebook/index.js
+++ client/src/components/Facebook/index.js
@@ -27,7 +27,7 @@ class LoginFacebook extends Component {
             imageLink: response.picture.data.url
         }})
         //get the user from our DB
-        const user = this.getThisUser()
+        const user = this.getThisUser(this.state.fbDetails.fb_ID)
         //if we do not find a user with that id we will create a user
         //for testing puposes we should make a bs id in order to see if it creates a new user
         !user ? createUser(this.state.fbDetails).then( res => console.log(res)).catch( err => console.log(err)) :
