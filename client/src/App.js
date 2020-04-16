f434305c8aadb7a9cc711182a480a7ed31cb90ef
Chris Lee <cil5345@gmail.com>
2020-04-16 09:09:52 -0400

    added firebase chat

--- client/src/App.js
+++ client/src/App.js
@@ -1,100 +1,109 @@
 import React, { Component } from "react";
-import Facebook from "./components/Facebook"//@HACER MOVE TO COMPONENETS //Mo commented out.  Talk with Mo
-import { BrowserRouter as Router, Route, Switch, Redirect } from "react-router-dom";
-import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
-import Profile from "./pages/Profile/Profile";
-import Home from './pages/Home'
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  Redirect
+} from "react-router-dom";
+import Home from "./pages/Home";
 import Chat from "./pages/Chat";
-import Signup from './pages/Signup'
-import Login from './pages/Login'
-import mainBG from "./components/mainBG/mainBG";
-import Matches from "./pages/Matches";
-import { auth } from './services/firebase';
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import { auth } from "./services/firebase";
+// import './styles.css';
 
 
+// private route with three props. If aut is true renders comp else goes to login page
 function PrivateRoute({ component: Component, authenticated, ...rest }) {
   return (
     <Route
       {...rest}
-      render={(props) => authenticated === true
-        ? <Component {...props} />
-        : <Redirect to={{ pathname: '/login', state: { from: props.location } }} />}
+      render={props =>
+        authenticated === true ? (
+          <Component {...props} />
+        ) : (
+            <Redirect
+              to={{ pathname: "/login", state: { from: props.location } }}
+            />
+          )
+      }
     />
-  )
+  );
 }
+// Public route with three props. If aut becomes true renders comp else goes to login page
 
 function PublicRoute({ component: Component, authenticated, ...rest }) {
   return (
     <Route
       {...rest}
-      render={(props) => authenticated === false
-        ? <Component {...props} />
-        : <Redirect to='/chat' />}
+      render={props =>
+        authenticated === false ? (
+          <Component {...props} />
+        ) : (
+            <Redirect to="/chat" />
+          )
+      }
     />
-  )
+  );
 }
-
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       authenticated: false,
-      loading: true,
+      loading: true
     };
   }
+  //mount phase, onAuthStateChanged triggers when aut state changes. updates state. User is initiall null
+  componentDidMount() {
+    auth().onAuthStateChanged(user => {
+      if (user) {
+        this.setState({
+          authenticated: true,
+          loading: false
+        });
+      } else {
+        this.setState({
+          authenticated: true,
+          loading: false
+        });
+      }
+    });
+  }
 
-
-componentDidMount() {
-  auth().onAuthStateChanged((user) => {
-    if (user) {
-      this.setState({
-        authenticated: true,
-        loading: false,
-      });
-    } else {
-      this.setState(
-        'authenticated'
-      //   {
-      //   authenticated: false,
-      //   loading: false,
-      // }
-      
+  render() {
+    return this.state.loading === true ? (
+      <div className="spinner-border text-success" role="status">
+        <span className="sr-only">Loading...</span>
+      </div>
+    ) : (
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <PrivateRoute
+              path="/chat"
+              authenticated={this.state.authenticated}
+              component={Chat}
+            />
+            <PublicRoute
+              path="/signup"
+              authenticated={this.state.authenticated}
+              component={Signup}
+            />
+            <PublicRoute
+              path="/login"
+              authenticated={this.state.authenticated}
+              component={Login}
+            />
+          </Switch>
+        </Router>
       );
-    }
-  })
-}
+  }
 
-render() {
-  return this.state.loading === true ? (
-    <div className="spinner-border text-success" role="status">
-      <span className="sr-only">Loading...</span>
-    </div>
-  ) : (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <PrivateRoute
-            path="/chat"
-            authenticated={this.state.authenticated}
-            component={Chat}
-          />
-          <PublicRoute
-            path="/signup"
-            authenticated={this.state.authenticated}
-            component={Signup}
-          />
-          <PublicRoute
-            path="/login"
-            authenticated={this.state.authenticated}
-            component={Login}
-          />
-        </Switch>
-      </Router>
-    );
-}
-}
-
+} //constructer 
 
 export default App;
+  
+
+  
--- client/src/services/firebase.js
+++ client/src/services/firebase.js
@@ -7,13 +7,10 @@ import firebase from 'firebase';
     apiKey: "AIzaSyADtPIiXsVSi1WP2hJxLUJP-dqCGuDTqbo",
     authDomain: "asteria-4f48d.firebaseapp.com",
     databaseURL: "https://asteria-4f48d.firebaseio.com",
-    projectId: "asteria-4f48d",
-    storageBucket: "asteria-4f48d.appspot.com",
-    messagingSenderId: "292050647789",
-    appId: "1:292050647789:web:cf0c4dd60c483296472ae4"
+    
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
-  
+
   export const auth = firebase.auth;
   export const db = firebase.database();
