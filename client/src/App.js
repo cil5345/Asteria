2dcfd86dd28477a1e9f45fd597abb1fcf45e8d06
Chris Lee <cil5345@gmail.com>
2020-04-16 10:44:08 -0400

    rever Firebase

--- client/src/App.js
+++ client/src/App.js
@@ -1,109 +1,100 @@
 import React, { Component } from "react";
-import {
-  Route,
-  BrowserRouter as Router,
-  Switch,
-  Redirect
-} from "react-router-dom";
-import Home from "./pages/Home";
-import Chat from "./pages/Chat";
-import Signup from "./pages/Signup";
-import Login from "./pages/Login";
-import { auth } from "./services/firebase";
-// import './styles.css';
+// import Header from "../components/Header";
+import { auth } from "../services/firebase";
+import { db } from "../services/firebase";
 
-
-// private route with three props. If aut is true renders comp else goes to login page
-function PrivateRoute({ component: Component, authenticated, ...rest }) {
-  return (
-    <Route
-      {...rest}
-      render={props =>
-        authenticated === true ? (
-          <Component {...props} />
-        ) : (
-            <Redirect
-              to={{ pathname: "/login", state: { from: props.location } }}
-            />
-          )
-      }
-    />
-  );
-}
-// Public route with three props. If aut becomes true renders comp else goes to login page
-
-function PublicRoute({ component: Component, authenticated, ...rest }) {
-  return (
-    <Route
-      {...rest}
-      render={props =>
-        authenticated === false ? (
-          <Component {...props} />
-        ) : (
-            <Redirect to="/chat" />
-          )
-      }
-    />
-  );
-}
-
-class App extends Component {
-  constructor() {
-    super();
+export default class Chat extends Component {
+  constructor(props) {
+    super(props);
     this.state = {
-      authenticated: false,
-      loading: true
+      user: auth().currentUser,
+      chats: [],
+      content: '',
+      readError: null,
+      writeError: null,
+      loadingChats: false
     };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.myRef = React.createRef();
   }
-  //mount phase, onAuthStateChanged triggers when aut state changes. updates state. User is initiall null
-  componentDidMount() {
-    auth().onAuthStateChanged(user => {
-      if (user) {
-        this.setState({
-          authenticated: true,
-          loading: false
+
+  async componentDidMount() {
+    this.setState({ readError: null, loadingChats: true });
+    const chatArea = this.myRef.current;
+    try {
+      db.ref("chats").on("value", snapshot => {
+        let chats = [];
+        snapshot.forEach((snap) => {
+          chats.push(snap.val());
         });
-      } else {
-        this.setState({
-          authenticated: true,
-          loading: false
-        });
-      }
+        chats.sort(function (a, b) { return a.timestamp - b.timestamp })
+        this.setState({ chats });
+        chatArea.scrollBy(0, chatArea.scrollHeight);
+        this.setState({ loadingChats: false });
+      });
+    } catch (error) {
+      this.setState({ readError: error.message, loadingChats: false });
+    }
+  }
+
+  handleChange(event) {
+    this.setState({
+      content: event.target.value
     });
   }
 
-  render() {
-    return this.state.loading === true ? (
-      <div className="spinner-border text-success" role="status">
-        <span className="sr-only">Loading...</span>
-      </div>
-    ) : (
-        <Router>
-          <Switch>
-            <Route exact path="/" component={Home} />
-            <PrivateRoute
-              path="/chat"
-              authenticated={this.state.authenticated}
-              component={Chat}
-            />
-            <PublicRoute
-              path="/signup"
-              authenticated={this.state.authenticated}
-              component={Signup}
-            />
-            <PublicRoute
-              path="/login"
-              authenticated={this.state.authenticated}
-              component={Login}
-            />
-          </Switch>
-        </Router>
-      );
+  async handleSubmit(event) {
+    event.preventDefault();
+    this.setState({ writeError: null });
+    const chatArea = this.myRef.current;
+    try {
+      await db.ref("chats").push({
+        content: this.state.content,
+        timestamp: Date.now(),
+        uid: this.state.user.uid
+      });
+      this.setState({ content: '' });
+      chatArea.scrollBy(0, chatArea.scrollHeight);
+    } catch (error) {
+      this.setState({ writeError: error.message });
+    }
   }
 
-} //constructer 
+  formatTime(timestamp) {
+    const d = new Date(timestamp);
+    const time = `${d.getDate()}/${(d.getMonth()+1)}/${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}`;
+    return time;
+  }
 
-export default App;
-  
+  render() {
+    return (
+      <div>
+        {/* <Header /> */}
 
-  
+        <div className="chat-area" ref={this.myRef}>
+          {/* loading indicator */}
+          {this.state.loadingChats ? <div className="spinner-border text-success" role="status">
+            <span className="sr-only">Loading...</span>
+          </div> : ""}
+          {/* chat area */}
+          {this.state.chats.map(chat => {
+            return <p key={chat.timestamp} className={"chat-bubble " + (this.state.user.uid === chat.uid ? "current-user" : "")}>
+              {chat.content}
+              <br />
+              <span className="chat-time float-right">{this.formatTime(chat.timestamp)}</span>
+            </p>
+          })}
+        </div>
+        <form onSubmit={this.handleSubmit} className="mx-3">
+          <textarea className="form-control" name="content" onChange={this.handleChange} value={this.state.content}></textarea>
+          {this.state.error ? <p className="text-danger">{this.state.error}</p> : null}
+          <button type="submit" className="btn btn-submit px-5 mt-4">Send</button>
+        </form>
+        <div className="py-5 mx-3">
+          Login in as: <strong className="text-info">{this.state.user.email}</strong>
+        </div>
+      </div>
+    );
+  }
+}
