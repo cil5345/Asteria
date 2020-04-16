32f89d23ae5fb596df6365e45f6a9e977496f6b7
Antonio Alvarado <ant.36alv@gmail.com>
2020-04-16 12:07:52 -0400

    proto login

---
+++ client/src/pages/Login/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Facebook from "../../components/Facebook";
+import LoginBG from "../../components/LoginBG/LoginBG";
+import Logo from "../../components/Logo/Logo";
+
+export default function Login() {
+    return (
+        <>
+        <LoginBG />
+        <Logo />
+        <Facebook />
+        </>
+    )
+}
+
