af8a042b925113ee93f12db23f53485711bedda9
Chris Lee <cil5345@gmail.com>
2020-04-16 23:06:43 -0400

    commit

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -50,9 +50,6 @@ class Profile extends Component {
         console.log(dk + " " + prefrences.value)
         console.log(dk + " " + gender.value)
         console.log(dk + " " + symbol.value)
-        console.log(dk + " " + sessionStorage.getItem("symbol"))
-        console.log(dk + " " + sessionStorage.getItem("gender"))
-        console.log(dk + " " + sessionStorage.getItem("prefrences"))
 
         sessionStorage.setItem("symbol", symbol.value)
         sessionStorage.setItem("gender", gender.value)
@@ -65,15 +62,20 @@ class Profile extends Component {
 
         this.updateUser()
 
+<<<<<<< HEAD
+        console.log("=============going to matches=============")
+        return <Redirect to="/Matches"/>
+=======
         console.log("going to matches")
         // return <Redirect to="/Matches"/>
+>>>>>>> eacf0173f006fd098b8933197be2c876ff36f840
     }
 
 
     componentDidMount = () => {
         if(sessionStorage.getItem("fid_pic")) {
 
-            console.log(`FB ID_PIC:${sessionStorage.getItem("fid_pic")}`)
+            console.log(`FB ID_PIC: ${sessionStorage.getItem("fid_pic")}`)
         }
     }
     componentWillMount = async () => {
@@ -155,8 +157,8 @@ class Profile extends Component {
                                     {/* <button className="successButton" onClick={this.getValues}>Get you're matches</button> */}
       {/* <button onClick={this.getValues}>colleeeeerado</button> */}
       
-<Link onClick={this.getValues} to={`/api/users/${this.state.id}/${this.state.symbol}/${this.state.gender}/${this.state.prefrence}`}>
-<button className="successButton">Get you’re matches</button>
+<Link to="/Matches">
+<button className="successButton" onClick={this.getValues}>Get you’re matches</button>
 </Link>
                                 </div>
                             </div>
