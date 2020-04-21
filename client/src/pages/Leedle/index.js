b62042fa4f83d51a34c487c92484fc5df9470936
Antonio Alvarado <ant.36alv@gmail.com>
2020-04-20 22:28:35 -0400

    the air was too heavy

--- client/src/pages/Leedle/index.js
+++ client/src/pages/Leedle/index.js
@@ -1,6 +1,31 @@
-import React from "react"
+import React, { useState } from "react"
 import "./style.css"
 
 export default function Leedle() {
-    return <h1>High Horse</h1>
+
+    const [ imageLink, setImageLink ] = useState("")
+    const [ newMatches, setNewMatches ] = useState([])
+
+    function change() {
+        alert("fkalf")
+    }
+
+    return  <>
+
+                <button>Go to matches</button>
+                <div id="div-pic">
+                    <img alt="You" src={imageLink}/>
+                    <button onClick={change}>Change</button>
+                </div>
+                <h1>High Horse</h1>
+                    <div id="matchesDiv">
+                        {newMatches.map( match => (
+                            <div className="match">
+                                {/* symbol */}
+                                <img alt={match.name} src={match.imageLink} />
+                                <p>{match.name}</p>
+                            </div>
+                        ))}
+                    </div>
+            </>
 }
