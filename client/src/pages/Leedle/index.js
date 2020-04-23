deca1008c2a2b3bca9bc0470b5bfedc367344dc2
Antonio Alvarado <ant.36alv@gmail.com>
2020-04-22 21:36:07 -0400

    via con dios

--- client/src/pages/Leedle/index.js
+++ client/src/pages/Leedle/index.js
@@ -30,8 +30,8 @@ export default function Leedle() {
         // .catch(err => console.log(err))
         console.log("tool")
         getNewMatches(sessionStorage.getItem("fb_ID"))
-            // .then(matches => setNewMatches([...matches]))
-            .then(matches => console.log(matches))
+            .then(matches => setNewMatches([...matches]))
+            // .then(matches => console.log(matches))
             .catch(err => console.log(err))
 
     }, [])
