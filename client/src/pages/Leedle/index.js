6e7f59484d5b43fff7daace3c491000663b355fb
Antonio Alvarado <ant.36alv@gmail.com>
2020-04-22 20:15:40 -0400

    the life the fuel the fire

--- client/src/pages/Leedle/index.js
+++ client/src/pages/Leedle/index.js
@@ -5,9 +5,9 @@ import "./style.css"
 
 export default function Leedle() {
 
-    const [ imageLink, setImageLink ] = useState("")
-    const [ user, setUser ] = useState({})
-    const [ newMatches, setNewMatches ] = useState([])
+    const [imageLink, setImageLink] = useState("")
+    const [user, setUser] = useState({})
+    const [newMatches, setNewMatches] = useState([])
 
     function change() {
         alert("fkalf")
@@ -17,13 +17,16 @@ export default function Leedle() {
         setImageLink(sessionStorage.getItem("imageLink"))
 
         getOneUser(sessionStorage.getItem("fb_ID"))
-            .then(u => console.log(u))
+            .then(u => {
+                console.log(u.data.fb_ID)
+                setUser(u.data)
+            })
             .catch(err => console.log(err))
         console.log(user)
         // setNewMatches([...getNewMatches(user.fb_ID)])
-            // .then(matches => setNewMatches())
-            // .catch(err => console.log(err))
-            console.log("tool")
+        // .then(matches => setNewMatches())
+        // .catch(err => console.log(err))
+        console.log("tool")
         getNewMatches(user.fb_ID)
             // .then(matches => setNewMatches([...matches]))
             .then(matches => console.log(matches))
@@ -31,22 +34,22 @@ export default function Leedle() {
 
     }, [])
 
-    return  <>
-                <Link to="/Matches"><button>Go to matches</button></Link>
-                {/* need a way to get details to matches */}
-                <div id="div-pic">
-                    <img alt="You" src={imageLink}/>
-                    <button id="btn" onClick={change}>Change</button>
+    return <>
+        <Link to="/Matches"><button>Go to matches</button></Link>
+        {/* need a way to get details to matches */}
+        <div id="div-pic">
+            <img alt="You" src={imageLink} />
+            <button id="btn" onClick={change}>Change</button>
+        </div>
+        <h1>High Horse</h1>
+        <div id="matchesDiv">
+            {newMatches.map(match => (
+                <div className="match">
+                    <p>{match.symbol}</p>
+                    <img alt={match.name} src={match.imageLink} />
+                    <p>{match.name}</p>
                 </div>
-                <h1>High Horse</h1>
-                    <div id="matchesDiv">
-                        {newMatches.map( match => (
-                            <div className="match">
-                                <p>{match.symbol}</p>
-                                <img alt={match.name} src={match.imageLink} />
-                                <p>{match.name}</p>
-                            </div>
-                        ))}
-                    </div>
-            </>
+            ))}
+        </div>
+    </>
 }
