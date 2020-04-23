9c0663c592561b3ac2ae801da20f7ac57c19ab8c
Antonio Alvarado <ant.36alv@gmail.com>
2020-04-22 22:18:00 -0400

    mind chekrers

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { HashRouter as Router, Route } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import Profile from "./pages/Profile/Profile";
 import Leedle from "./pages/Leedle"
