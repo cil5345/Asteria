5543af204130f32fb86a2f119ae295e2a21eafa5
Antonio Alvarado <ant.36alv@gmail.com>
2020-04-24 19:39:16 -0400

    ladybird

--- client/src/components/PhotoUpload/index.js
+++ client/src/components/PhotoUpload/index.js
@@ -6,7 +6,7 @@ export default function PhotoUpload() {
 
     //want to fancy this up
 
-    return <form method="POST" action="/photo/upload" enctype="multipart/form-data">>
+    return <form method="POST" action={`photo/upload/${sessionStorage.getItem("fb_ID")}`} enctype="multipart/form-data">>
                 <input type="file" name="photo" />
                 <button type="submit">Submit</button>
             </form>
