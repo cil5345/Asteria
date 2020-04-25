166a1da478b9d947b3439443ad7d788c979d9202
Antonio Alvarado <ant.36alv@gmail.com>
2020-04-25 12:48:30 -0400

    ladybird

--- routes/photoUpload.js
+++ routes/photoUpload.js
@@ -1,32 +1,32 @@
-const multer = require("multer")
-const path = require("path")
+// const multer = require("multer")
+// const path = require("path")
 
-module.exports = function(app) {
+// module.exports = function(app) {
 
-const storage = multer.diskStorage({
-    destination: "../uploads",
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + path.extname(file.originalname))
-    }
-})
+// const storage = multer.diskStorage({
+//     destination: "../uploads",
+//     filename: (req, file, cb) => {
+//         cb(null, file.fieldname + path.extname(file.originalname))
+//     }
+// })
 
-const upload = multer({ 
-    storage: storage
-}).single("photo")
+// const upload = multer({ 
+//     storage: storage
+// }).single("photo")
 
-//dest: "../uploads/" })
+// //dest: "../uploads/" })
     
-    app.post("/photo/upload/:id", upload, (req, res, next) => {
+//     app.post("/photo/upload/:id", upload, (req, res, next) => {
 
-        upload(req, res, (err) => {
-            if(err) return console.log(err)
+//         upload(req, res, (err) => {
+//             if(err) return console.log(err)
 
-            console.log(`hit server route for photo upload`)
-            console.log(`${req.file}`)
-            console.log(`${req.file.fieldname}`)
-            console.log(`${req.file.path}`)
-            console.log(`${req.params.id}`)
-            console.log(req.file)
-        })
-    })
-}
+//             console.log(`hit server route for photo upload`)
+//             console.log(`${req.file}`)
+//             console.log(`${req.file.fieldname}`)
+//             console.log(`${req.file.path}`)
+//             console.log(`${req.params.id}`)
+//             console.log(req.file)
+//         })
+//     })
+// }
