03c426688f6b36e7991dce71cf90f7899adc09be
Antonio Alvarado <ant.36alv@gmail.com>
2020-04-30 18:05:49 -0400

    propane and propane accescories

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ if(process.env.NODE_ENV === "production") {
 
 require("./routes/mongo-routes")(app)
 require("./routes/test-routes")(app)
-require("./routes/photo-uwpload")(app)
+require("./routes/photo-upload")(app)
 
 // Connect to the Mongo DB
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/asteriaDB", { useNewUrlParser: true });
