822bf6ca70735020431bd71c294e5568927c4213
Antonio Alvarado <ant.36alv@gmail.com>
2020-04-30 18:38:51 -0400

    propane and propane accescories

--- routes/photo-upload.js
+++ routes/photo-upload.js
@@ -30,7 +30,7 @@ module.exports = function (app) {
 
 
         console.log(`${req.file}`)
-        console.log(`${req.file.fieldname}`)
+        // console.log(`${req.file.fieldname}`)
         console.log(`${req.file.path}`)
         console.log(`${req.params.file}`)
         console.log(`${req.params}`)
