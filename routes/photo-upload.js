4333bf209c8172182df136e72f5dbc4a2f435a7e
Antonio Alvarado <ant.36alv@gmail.com>
2020-04-30 18:34:24 -0400

    propane and propane accescories

--- routes/photo-upload.js
+++ routes/photo-upload.js
@@ -1,33 +1,39 @@
-const multer = require("multer")
-const path = require("path")
+// const multer = require("multer")
+// const path = require("path")
 
-const storage = multer.diskStorage({
-    destination: "../uploads",
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + path.extname(file.originalname))
-    }
-})
+// const storage = multer.diskStorage({
+//     destination: "../uploads",
+//     filename: (req, file, cb) => {
+//         cb(null, file.fieldname + path.extname(file.originalname))
+//     }
+// })
 
-const upload = multer({ 
-    storage: storage
-}).single("photo")
+// const upload = multer({ 
+//     storage: storage
+// }).single("photo")
 
-// dest: "../uploads/" })
+//dest: "../uploads/" })
 module.exports = function (app) {
-    
+
     app.post("/photo/upload/:id", (req, res) => {
 
-        upload(req, res, (err) => {
-            if(err) return console.log(err)
+        // upload(req, res, (err) => {
+        //     if(err) return console.log(err)
 
-            console.log(`hit server route for photo upload`)
-            console.log(`${req.file}`)
-            console.log(`${req.file.fieldname}`)
-            console.log(`${req.file.path}`)
-            console.log(`${req.params.id}`)
-            console.log(req.file)
-        })
+        //     console.log(`hit server route for photo upload`)
+        //     console.log(`${req.file}`)
+        //     console.log(`${req.file.fieldname}`)
+        //     console.log(`${req.file.path}`)
+        //     console.log(`${req.params.id}`)
+        //     console.log(req.file)
+        // })
 
+
+        console.log(`${req.file}`)
+        console.log(`${req.file.fieldname}`)
+        console.log(`${req.file.path}`)
+        console.log(`${req.params.file}`)
+        console.log(`${req.params}`)
         res.send("ok")
     })
 }
