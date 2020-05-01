5a35e480b33259cddbd198eb69a3e9a67ebc33a4
Antonio Alvarado <ant.36alv@gmail.com>
2020-05-01 19:21:57 -0400

    propane and propane accescories

--- routes/photo-upload.js
+++ routes/photo-upload.js
@@ -20,29 +20,29 @@ module.exports = function (app) {
     // app.post("/photo/upload/:id", upload.single("profile_picture"), (err, req, res, next) => {
     app.post("/photo/upload/:id", (req, res) => {
 
-        upload(req, res, (err) => {
-            if(err) return console.log(err)
+        // upload(req, res, (err) => {
+        //     if(err) return console.log(err)
 
-            console.log(`hit server route for photo upload`)
-            console.log(`${req.file}`)
-            console.log(`${req.file.fieldname}`)
-            console.log(`${req.file.path}`)
-            console.log(`${req.params.id}`)
-            console.log(req.file)
-        })
-        // // for(let b of req.params) console.log(b)
+        //     console.log(`hit server route for photo upload`)
+        //     console.log(`${req.file}`)
+        //     console.log(`${req.file.fieldname}`)
+        //     console.log(`${req.file.path}`)
+        //     console.log(`${req.params.id}`)
+        //     console.log(req.file)
+        // })
+        // for(let b of req.params) console.log(b)
 
-        // console.log("we in err")
+        console.log("we in err")
         
-        // // if(err) console.log(err)
-        // // console.log(req.file.buffer)
-        // console.log("file")
-        // // console.log(req.file.buffer)
-        // console.log("body")
-        // console.log(req.body.file)
-        // // console.log(req)
-        // // console.log(`${req.file}`)
-        // console.log(req.files)
+        // if(err) console.log(err)
+        // console.log(req.file.buffer)
+        console.log("file")
+        // console.log(req.file.buffer)
+        console.log("body")
+        console.log(req.body.file)
+        // console.log(req)
+        // console.log(`${req.file}`)
+        console.log(req.files)
         res.send("ok")
     })
 }
