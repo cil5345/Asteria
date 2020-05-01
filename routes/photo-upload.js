92b571d6594e0f142d8dde948eec640bb05b0297
Antonio Alvarado <ant.36alv@gmail.com>
2020-04-30 21:01:38 -0400

    propane and propane accescories

--- routes/photo-upload.js
+++ routes/photo-upload.js
@@ -1,12 +1,12 @@
-const multer = require("multer")
-const path = require("path")
+// const multer = require("multer")
+// const path = require("path")
 
-const storage = multer.diskStorage({
-    destination: "../uploads",
-    filename: (req, file, cb) => {
-        cb(null, file.fieldname + path.extname(file.originalname))
-    }
-})
+// const storage = multer.diskStorage({
+//     destination: "../uploads",
+//     filename: (req, file, cb) => {
+//         cb(null, file.fieldname + path.extname(file.originalname))
+//     }
+// })
 
 // const upload = multer({ 
 //     storage: storage
