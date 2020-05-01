47e5b27b8da8ac9beea5d6dca51711ffb9a798ea
Antonio Alvarado <ant.36alv@gmail.com>
2020-04-30 22:23:22 -0400

    propane and propane accescories

--- routes/photo-upload.js
+++ routes/photo-upload.js
@@ -1,21 +1,23 @@
-// const multer = require("multer")
-// const path = require("path")
+const multer = require("multer")
+const path = require("path")
 
-// const storage = multer.diskStorage({
-//     destination: "../uploads",
-//     filename: (req, file, cb) => {
-//         cb(null, file.fieldname + path.extname(file.originalname))
-//     }
-// })
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "../uploads/")
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
 
-// const upload = multer({ 
-//     storage: storage
-// }).single("photo")
+
+
+const upload = multer({ storage: storage })//.single("photo")
 
 //dest: "../uploads/" })
 module.exports = function (app) {
 
-    app.post("/photo/upload/:id", (req, res) => {
+    app.post("/photo/upload/:id", upload.single("profile_picture"), (req, res, next) => {
 
         // upload(req, res, (err) => {
         //     if(err) return console.log(err)
@@ -29,11 +31,11 @@ module.exports = function (app) {
         // })
 
 
+        console.log(req)
         console.log(`${req.file}`)
         console.log(`${req.files}`)
         // console.log(`${req.file.fieldname}`)
         // console.log(`${req.file.path}`)
-        console.log(req)
         console.log(req.params)
         console.log(`${req.params.file}`)
         console.log(`${JSON.stringify(req.params)}`)
