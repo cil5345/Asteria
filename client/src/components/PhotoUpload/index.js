291a5248c5d5db1570d9e6c3050615b855366b73
Antonio Alvarado <ant.36alv@gmail.com>
2020-05-03 21:39:48 -0400

    propane and propane accescories

--- client/src/components/PhotoUpload/index.js
+++ client/src/components/PhotoUpload/index.js
@@ -42,7 +42,7 @@ export default function PhotoUpload(props) {
                         //ie /api/user/*fileName.ext*
                         const uploadedPhotoName = imLink.substring(imLink.lastIndexOf("/") + 1, imLink.length)
 
-                        setImagesToShow(uploadedPhotoName)
+                        setImagesToShow([...imagesToShow], getImage(uploadedPhotoName))
                         
                         console.log("donezo")
                     })
