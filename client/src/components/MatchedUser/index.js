e21f76e2b098868b38d35b9b193634235266457a
Antonio Alvarado <ant.36alv@gmail.com>
2020-05-04 18:07:51 -0400

    esta juan occupado

--- client/src/components/MatchedUser/index.js
+++ client/src/components/MatchedUser/index.js
@@ -1,12 +1,12 @@
 import React from "react"
 import "./style.css"
 
-export const MatchedUser = props => (
-    
+const MatchedUser = props => (
     <div className="match">
             <img alt={props.symbol} src={`/api/image/${props.symbol.toLowerCase()}`} />
             <img alt={props.name} src={props.imageLink} />
         <span>{props.name}</span>
     </div>
-    
     )
+
+export default MatchedUser
