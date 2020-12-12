0469205f9f9f64cdf2b6ee94968cd5688534413c
antonio36alv <ant.36alv@gmail.com>
2020-12-12 14:31:04 -0500

    style changes and code clean up

--- client/src/components/MatchedUser/index.js
+++ client/src/components/MatchedUser/index.js
@@ -1,15 +1,38 @@
 import React from "react"
+import Avatar from '@material-ui/core/Avatar';
+import { makeStyles } from '@material-ui/core/styles';
 import "./style.css"
 
-const MatchedUser = props => (
-    <div className="match">
-            <img alt={props.name} src={props.imageLink} />
-            <div className="match-details-div">
-                <span>{props.name}</span>
-                <span>{props.symbol}</span>
-            </div>
-            <img alt={props.symbol} src={`/api/image/${props.symbol.toLowerCase()}`} />
-    </div>
-    )
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+   shape: {
+    backgroundColor: theme.palette.primary.main,
+    width: 40,
+    height: 40,
+  },
+  large: {
+      minHeight: "100px",
+      minWidth: "100px"
+  }
+}));
+
+const MatchedUser = props => {
+const classes = useStyles();
+return (
+  <div className="match">
+          <Avatar alt={props.name} src={props.imageLink} className={classes.large} />
+          <div className="match-details-div">
+              <span>{props.name}</span>
+              <span>{props.symbol}</span>
+          </div>
+          <img alt={props.symbol} src={`/api/image/${props.symbol.toLowerCase()}`} />
+  </div>
+  )
+}
 
 export default MatchedUser
